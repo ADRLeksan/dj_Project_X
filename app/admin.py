1733919774992f09814156233325d847fd40a96d
Alex <Leksan2012@gmail.com>
2024-04-20 19:10:24 +0300

    phase 3

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
+import inspect
 from django.contrib import admin
+import app.models
 
 # Register your models here.
+ms = inspect.getmembers(app.models, inspect.isclass)
+for model in ms: admin.site.register(model[1])
